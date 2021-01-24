8702e6c6b9cad4c055de6669e9b9ed77ca0fd5de
Yesimyc <54077087+Yesimyc@users.noreply.github.com>
2021-01-23 23:16:07 -0600

    Update Helvaci.java

--- src/test/java/com/crmly/pages/Helvaci.java
+++ src/test/java/com/crmly/pages/Helvaci.java
@@ -3,9 +3,13 @@ package com.crmly.pages;
 public class Helvaci {
 
     public static void main(String[] args) {
+        int c =10;
+        int d =20;
         int a =5;
         int b =10;
         System.out.println(a+b);
+         System.out.println(c+d);
+        
     }
 }
 
